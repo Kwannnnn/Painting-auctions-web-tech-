b8db992d6d142370a1d73affe4904365661f02e0
Quan <trungquann2412@gmail.com>
2021-09-13 16:22:21 +0200

    added some folders/files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const app = express();
 
 const port = 3000;
 
-app.listen(port, function(){
+app.use(express.json());
+
+app.use('/paintings', require('./routes/paintings'));
+
+app.listen(port, () => {
     console.log(`API is running on port ${port}!`);
 })
 
--- src/routes/paintings.js
+++ src/routes/paintings.js
@@ -1,9 +1,10 @@
 const express = require("express");
-// const { StatusCodes } = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 const router = express.Router();
 
 router.get('../paintings.json', (req , res) => {
     res.send("this is supposed to be a list of all paintings");
+    res.status(StatusCodes.OK);
 });
 
 module.exports = router;
