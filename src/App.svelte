797c95af9259ecb7b039f0229f3646dcf6d2c35b
Mira <497124@student.saxion.nl>
2021-10-07 16:02:59 +0200

    added routing and pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,30 @@
 <script>
-	export let name;
+    import router from "page";
+    import LogIn from "./pages/LogIn.svelte";
+    import Main from "./pages/Main.svelte";
+    import Profile from "./pages/Profile.svelte";
+    import Item from "./pages/Item.svelte";
+
+    let page;
+    let params;
+
+    router('/login', (ctx) => page = LogIn);
+    router('/main', (ctx) => page = Main);
+    router('/profile/:id', (ctx) => page = Profile);
+    router('/item/:id', (ctx) => page = Item);
+
+    router.start();
 </script>
 
+<svelte:component this="{page}" {params}/>
+
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
 </main>
 
 <style lang="postcss" global>
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
-</style>
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ src/pages/Main.svelte
@@ -0,0 +1,7 @@
+<script>
+    import HeaderComponent from "../components/HeaderComponent.svelte";
+
+</script>
+
+<HeaderComponent/>
+<h1>Have a nice day</h1>
