c4ee7a7be4f31802b265e0ae76cce322daa5f8c3
Kwannnn <490848@student.saxion.nl>
2021-10-14 22:58:12 +0200

    Completed tests yay!

--- src/routes/bids.js
+++ src/routes/bids.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/bids');
 const {validateResult, bidValidators} = require("../middleware/validate-input");
 const isLoggedIn = require("../middleware/is-logged-in");
+const isAdmin = require("../middleware/is-admin");
 
 
 router.get("", controller.getBids);
@@ -11,7 +12,7 @@ router.get("/:id", controller.getOneBid);
 
 router.post("", isLoggedIn, bidValidators, validateResult, controller.addBid);
 
-router.delete("/:id", isLoggedIn, controller.deleteBid);
+router.delete("/:id", isLoggedIn, isAdmin, controller.deleteBid);
 
 
 module.exports = router;
